a218dcd7c83269e149778e95378af967a7715e43
Judy-Liang <66293718+Judy-Liang@users.noreply.github.com>
2024-02-05 00:16:22 -0800

    Add files via upload
    Main_with_arg

--- src/main/java/edu/ucsd/xmlqueryprocessor/Main.java
+++ src/main/java/edu/ucsd/xmlqueryprocessor/Main.java
@@ -1,54 +1,49 @@
 package edu.ucsd.xmlqueryprocessor;
 
 import edu.ucsd.xmlqueryprocessor.engine.Engine;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
 
 public class Main {
-    // Find all persons in the play
-    public static final String SAMPLE_QUERY_1 = "doc(\"j_caesar.xml\")//PERSONA";
-
-    // Find the scenes in which CAESAR speaks
-    public static final String SAMPLE_QUERY_2 = "doc(\"j_caesar.xml\")//SCENE[SPEECH/SPEAKER/text() = \"CAESAR\"]";
-
-    // Find the acts containing some scene in which both CAESAR and BRUTUS speak
-    public static final String SAMPLE_QUERY_3 = "doc(\"j_caesar.xml\")//ACT[SCENE[SPEECH/SPEAKER/text() = \"CAESAR\"][SPEECH/SPEAKER/text() = \"BRUTUS\"]]";
-
-    // Same as previous, but different syntax
-    public static final String SAMPLE_QUERY_4 = "doc(\"j_caesar.xml\")//ACT[.//SPEAKER/text() = \"CAESAR\"][.//SPEAKER/text() = \"BRUTUS\"]";
-
-    // Find the acts in which CAESAR no longer appears
-    public static final String SAMPLE_QUERY_5 = "doc(\"j_caesar.xml\")//ACT[not .//SPEAKER/text() = \"CAESAR\"]";
-
-    public static final String FILE_DIRECTORY = "/Users/keranwang/Desktop/Winter 24/CSE 232B/project/xquery-processor/";
-    public static final String OUTPUT_FILE_DIRECTORY = "/Users/keranwang/Desktop/Winter 24/CSE 232B/project/xquery-processor/output/";
+    // run in sh: java Main "D://Java-code/CSE232b/milestone1/xquery-processor-master/" \
+    // "D://Java-code/CSE232b/milestone1/xquery-processor-master/test.txt.txt" \
+    // "D://Java-code/CSE232b/milestone1/xquery-processor-master/output/"
 
     public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: java Main <FILE_DIRECTORY> <QUERY_FILE_DIRECTORY> <OUTPUT_FILE_DIRECTORY>");
+            return;
+        }
+
+        String FILE_DIRECTORY = args[0];
+        String QUERY_FILE_DIRECTORY = args[1];
+        String OUTPUT_FILE_DIRECTORY = args[2];
+
+//        String FILE_DIRECTORY = "D://Java-code/CSE232b/milestone1/xquery-processor-master/";
+//        String QUERY_FILE_DIRECTORY = "D://Java-code/CSE232b/milestone1/xquery-processor-master/test.txt";
+//        String OUTPUT_FILE_DIRECTORY = "D://Java-code/CSE232b/milestone1/xquery-processor-master/output/";
+
         Engine engine = new Engine(FILE_DIRECTORY, OUTPUT_FILE_DIRECTORY);
-        System.out.println("Find all persons in the play");
-        System.out.println("SAMPLE QUERY 1:\t" + SAMPLE_QUERY_1);
-        engine.process(SAMPLE_QUERY_1, "result1");
-        System.out.println("--------------------------------------------------------------------------------");
-        System.out.println("--------------------------------------------------------------------------------");
 
-        System.out.println("Find the scenes in which CAESAR speaks");
-        System.out.println("SAMPLE QUERY 2:\t" + SAMPLE_QUERY_2);
-        engine.process(SAMPLE_QUERY_2, "result2");
-        System.out.println("--------------------------------------------------------------------------------");
-        System.out.println("--------------------------------------------------------------------------------");
+        // read and process test XQueries
+        readProcessQueries(engine, QUERY_FILE_DIRECTORY);
+    }
 
-        System.out.println("Find the acts containing some scene in which both CAESAR and BRUTUS speak");
-        System.out.println("SAMPLE QUERY 3:\t" + SAMPLE_QUERY_3);
-        engine.process(SAMPLE_QUERY_3, "result3");
-        System.out.println("--------------------------------------------------------------------------------");
-        System.out.println("--------------------------------------------------------------------------------");
-
-        System.out.println("Same as previous, but different syntax");
-        System.out.println("SAMPLE QUERY 4:\t" + SAMPLE_QUERY_4);
-        engine.process(SAMPLE_QUERY_4, "result4");
-        System.out.println("--------------------------------------------------------------------------------");
-        System.out.println("--------------------------------------------------------------------------------");
-
-        System.out.println("Find the acts in which CAESAR no longer appears");
-        System.out.println("SAMPLE QUERY 5:\t" + SAMPLE_QUERY_5 + "\n");
-        engine.process(SAMPLE_QUERY_5, "result5");
+    private static void readProcessQueries(Engine engine, String queryFilePath) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(queryFilePath));
+            int queryCounter = 1;
+            for (String query : lines) {
+                System.out.println("SAMPLE QUERY " + queryCounter + ":\t" + "SAMPLE_QUERY_" + queryCounter);
+                String resultIdentifier = "result" + queryCounter++;
+                engine.process(query, resultIdentifier);
+                System.out.println("--------------------------------------------------------------------------------");
+                System.out.println("--------------------------------------------------------------------------------");
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading queries file: " + e.getMessage());
+        }
     }
 }
