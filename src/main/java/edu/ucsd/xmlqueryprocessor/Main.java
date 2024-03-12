88a9245970a67ad154520feade98bc8ac4ef2cac
Keran Wang <83668340+keran-w@users.noreply.github.com>
2024-03-11 21:14:36 -0700

    Bug fix for let clause and join operator (#13)
    * Update grammar and fix bugs
    
    * Ignore blank line difference
    
    * Refractor code base

--- src/main/java/edu/ucsd/xmlqueryprocessor/Main.java
+++ src/main/java/edu/ucsd/xmlqueryprocessor/Main.java
@@ -15,49 +15,72 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        int milestone = 3;
+
         int start = 1;
-        int queryCount = 4;
+        int queryCount = 20;
 
-        System.out.println("--------------------------------------------------------------------------------");
-        for (int index = start; index < start + queryCount; index++) {
-            try {
-                String outputDir = String.format("output/test%d/", milestone);
-                Path outputPath = Paths.get(outputDir);
-                XQueryEngine engine = new XQueryEngine("data/", outputDir);
-                if (!Files.exists(outputPath)) {
-                    Files.createDirectory(outputPath);
-                }
-
-                // System.out.printf("Testing query %d%n", index);
-                String query;
-                Path filepath;
-                if (milestone == 3) {
-                    filepath = Paths.get(String.format("input/m%d-rewrite/query%d.txt", milestone, index));
-//                    filepath = Paths.get(String.format("input/m%d/query%d.txt", milestone, index));
-                    query = Files.readString(filepath);
-                } else {
-                    filepath = Paths.get(String.format("input/m%d/m%d-test.txt", milestone, milestone));
-                    query = Files.readAllLines(filepath).get(index - 1);
-                }
-                System.out.println("Processing query " + index);
-                // measure time in seconds taken
-                long startTime = System.currentTimeMillis();
-                engine.evaluate(query, String.format("result%d.xml", index));
-                long endTime = System.currentTimeMillis();
-                System.out.println("Time taken: " + (endTime - startTime) / 1000.0 + " seconds");
-
-                System.out.printf("Successfully processed query %d!%n", index);
+        for (int milestone = 1; milestone <= 3; milestone++) {
+            System.out.println("--------------------------------------------------------------------------------");
+            for (int index = start; index < start + queryCount; index++) {
                 try {
-                    FileComparer.compareFiles(
-                            String.format("output/test%d/result%d.xml", milestone, index),
-                            String.format("output/m%d/result%d.xml", milestone, index)
-                    );
+                    String outputDir = String.format("output/test%d/", milestone);
+                    Path outputPath = Paths.get(outputDir);
+                    XQueryEngine engine = new XQueryEngine("data/", outputDir);
+                    if (!Files.exists(outputPath)) {
+                        Files.createDirectory(outputPath);
+                    }
+
+                    String query = null;
+                    Path inputFilepath = null;
+
+                    switch (milestone) {
+                        case 1:
+                            inputFilepath = Paths.get(String.format("input/m%d/m%d-test.txt", milestone, milestone));
+                            query = Files.readAllLines(inputFilepath).get(index - 1);
+                            break;
+                        case 2:
+                            inputFilepath = Paths.get(String.format("input/m%d/query%d.txt", milestone, index));
+                            query = Files.readString(inputFilepath);
+                            break;
+                        case 3:
+                            inputFilepath = Paths.get(String.format("input/m%d-rewrite/query%d.txt", milestone, index));
+                            query = Files.readString(inputFilepath);
+                            break;
+                    }
+
+                    if (query == null) {
+                        break;
+                    }
+                    System.out.println("Processing Milestone " + milestone + " Query " + index);
+
+
+                    String outputFilename = String.format("result%d.xml", index);
+                    String outputFilepath = String.format("output/test%d/%s", milestone, outputFilename);
+                    String testFilepath = String.format("output/m%d/%s", milestone, outputFilename);
+
+                    System.out.println("Input file: " + inputFilepath);
+                    System.out.println("Output file: " + outputFilepath);
+                    System.out.println("Test file: " + testFilepath);
+
+                    // measure time in seconds taken
+                    long startTime = System.currentTimeMillis();
+                    engine.evaluate(query, outputFilename);
+                    long endTime = System.currentTimeMillis();
+                    System.out.println("Time taken: " + (endTime - startTime) / 1000.0 + " seconds");
+
+                    System.out.printf("Successfully processed query %d!%n", index);
+                    try {
+                        FileComparer.compareFiles(
+                                String.format("output/test%d/result%d.xml", milestone, index),
+                                String.format("output/m%d/result%d.xml", milestone, index)
+                        );
+                    } catch (Exception ignored) {
+                    }
+                    System.out.println("--------------------------------------------------------------------------------");
                 } catch (Exception ignored) {
+                    // ignored.printStackTrace();
+                    break;
                 }
-                System.out.println("--------------------------------------------------------------------------------");
-            } catch (Exception e) {
-                e.printStackTrace();
             }
         }
     }
